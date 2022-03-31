9bc5c125a0fc6418e182c5658bcfa58588d9fee7
san4ous123 <78911591+san4ous123@users.noreply.github.com>
2022-03-31 21:37:38 +0700

    hw

---
+++ lab7/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace lab7.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
